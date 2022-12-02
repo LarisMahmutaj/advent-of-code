c6374fb45355fd7b666ec0f2670a49356fb0c8fe
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2022-12-02 11:56:10 +0100

    Get project path dynamically in FileReaderUtil

--- src/utils/FileReaderUtil.java
+++ src/utils/FileReaderUtil.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,8 +9,10 @@ public class FileReaderUtil {
     public static String readString(Object day) {
         try {
             String year = day.getClass().getName().split("\\.")[0];
+            File currentDir = new File("");
+            String projectPath = currentDir.getAbsolutePath();
             Path path = Path.of(
-                    "/Users/laris/Projects/advent-of-code/src/" + year + "/days/" + day.getClass()
+                    projectPath + "/src/" + year + "/days/" + day.getClass()
                             .getSimpleName() + "/Input.txt");
             return Files.readString(path);
         } catch (IOException e) {
