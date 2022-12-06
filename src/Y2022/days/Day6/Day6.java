5cc9ad0db0c0d75ccf3626419f35eab2df5e9016
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2022-12-07 00:33:32 +0100

    D5 & D6 of 2022 done (don't look at day 5)

---
+++ src/Y2022/days/Day6/Day6.java
@@ -0,0 +1,38 @@
+package Y2022.days.Day6;
+
+import interfaces.Day;
+import utils.FileReaderUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day6 implements Day {
+    @Override
+    public void partOne() {
+        execute(4);
+    }
+
+    @Override
+    public void partTwo() {
+        execute(14);
+    }
+    
+    private void execute(int distinctCharacterNumber){
+        String input = FileReaderUtil.readString(this);
+
+        int marker = 0;
+        for (int i = 0; i < input.length() - distinctCharacterNumber; i++) {
+            String sub = input.substring(i, i + distinctCharacterNumber);
+
+            List<String> chars = Arrays.stream(sub.split("")).toList();
+            chars = chars.stream().distinct().collect(Collectors.toList());
+            if (chars.size() == distinctCharacterNumber) {
+                marker = i + distinctCharacterNumber;
+                break;
+            }
+        }
+
+        System.out.println(marker);
+    }
+}
